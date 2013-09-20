rabbitircd/rabbitircd

commit 8000fa002653cd7d21c1834458c87d34926b72c0

expunge remote includes (closes #4)

src/ircd.c
@@ -60,8 +60,6 @@ Computing Center and Jarkko Oikarinen";
 #endif
 #include "version.h"
 #include "proto.h"
-#ifdef USE_LIBCURL
-#include <curl/curl.h>
 #endif
 ID_Copyright
     ("(C) 1988 University of Oulu, Computing Center and Jarkko Oikarinen");
@@ -92,10 +90,6 @@ MODVAR char *me_hash;
 extern char backupbuf[8192];
 
 unsigned char conf_debuglevel = 0;
-
-#ifdef USE_LIBCURL
-extern void url_init(void);
-#endif
 
 time_t highesttimeofday=0, oldtimeofday=0, lasthighwarn=0;
 
@@ -666,33 +660,6 @@ int error = 0;
 		error=1;
 	}
 #endif
-#ifdef USE_LIBCURL
-	/* Perhaps someone should tell them to do this a bit more easy ;)
-	 * problem is runtime output is like: 'libcurl/7.11.1 c-ares/1.2.0'
-	 * while header output is like: '7.11.1'.
-	 */
-	{
-		char buf[128], *p;
-		
-		runtime = curl_version();
-		compiledfor = LIBCURL_VERSION;
-		if (!strncmp(runtime, "libcurl/", 8))
-		{
-			strlcpy(buf, runtime+8, sizeof(buf));
-			p = strchr(buf, ' ');
-			if (p)
-			{
-				*p = '\0';
-				if (strcmp(compiledfor, buf))
-				{
-					version_check_logerror("Curl version mismatch: compiled for '%s', library is '%s'",
-						compiledfor, buf);
-					error = 1;
-				}
-			}
-		}
-	}
-#endif
 
 	if (error)
 	{
@@ -980,9 +947,6 @@ int main(int argc, char *argv[])
 	mp_pool_init();
 	dbuf_init();
 
-#ifdef USE_LIBCURL
-	url_init();
-#endif
 	tkl_init();
 	umode_init();
 	extcmode_init();
@@ -1145,8 +1109,6 @@ int main(int argc, char *argv[])
 	}
 #endif
 	mkdir("tmp", S_IRUSR|S_IWUSR|S_IXUSR); /* Create the tmp dir, if it doesn't exist */
-#if defined(USE_LIBCURL) && defined(REMOTEINC_SPECIALCACHE)
- 	mkdir("cache", S_IRUSR|S_IWUSR|S_IXUSR); /* Create the cache dir, if using curl and it doesn't exist */
 #endif
 	/*
 	 * didn't set debuglevel 
@@ -1167,9 +1129,6 @@ int main(int argc, char *argv[])
 	fprintf(stderr, "                     using %s\n", tre_version());
 #ifdef USE_SSL
 	fprintf(stderr, "                     using %s\n", SSLeay_version(SSLEAY_VERSION));
-#endif
-#ifdef USE_LIBCURL
-	fprintf(stderr, "                     using %s\n", curl_version());
 #endif
 	fprintf(stderr, "\n");
 	clear_client_hash_table();

src/s_conf.c
@@ -389,11 +389,6 @@ MODVAR int			config_error_flag = 0;
 int			config_verbose = 0;
 
 void add_include(const char *filename, const char *included_from, int included_from_line);
-#ifdef USE_LIBCURL
-void add_remote_include(const char *, const char *, int, const char *, const char *included_from, int included_from_line);
-void update_remote_include(ConfigItem_include *inc, const char *file, int, const char *errorbuf);
-int remote_include(ConfigEntry *ce);
-#endif
 void unload_notloaded_includes(void);
 void load_includes(void);
 void unload_loaded_includes(void);
@@ -1476,25 +1471,6 @@ int config_test_openfile(ConfigEntry *cep, int flags, mode_t mode, const char *e
 		return 1;
 	}
 
-	/* There's not much checking that can be done for asynchronously downloaded files */
-#ifdef USE_LIBCURL
-	if(url_is_valid(cep->ce_vardata))
-	{
-		if(allow_url)
-			return 0;
-
-		/* but we can check if a URL is used wrongly :-) */
-		config_warn("%s:%i: %s: %s: URL used where not allowed",
-			    cep->ce_fileptr->cf_filename,
-			    cep->ce_varlinenum,
-			    entry, cep->ce_vardata);
-		if(fatal)
-			return 1;
-		else
-			return 0;
-	}
-#endif /* USE_LIBCURL */
-
 	/* 
 	 * Make sure that files are created with the correct mode. This is 
 	 * because we don't feel like unlink()ing them...which would require
@@ -1937,13 +1913,6 @@ int	load_conf(char *filename, const char *original_path)
 			counter ++;
 			continue;
 		}
-#ifdef USE_LIBCURL
-		if (inc->url && !strcmp(original_path, inc->url))
-		{
-			counter ++;
-			continue;
-		}
-#endif
 	}
 	if (counter > 1 || my_inc->flag.type & INCLUDE_USED)
 	{
@@ -3100,11 +3069,6 @@ int	_conf_include(ConfigFile *conf, ConfigEntry *ce)
 			ce->ce_varlinenum);
 		return -1;
 	}
-#ifdef USE_LIBCURL
-	if (url_is_valid(ce->ce_vardata))
-		return remote_include(ce);
-#endif
-#if !defined(OSXTIGER) && DEFAULT_PERMISSIONS != 0
 	chmod(ce->ce_vardata, DEFAULT_PERMISSIONS);
 #endif
 #ifdef GLOBH
@@ -9489,118 +9453,10 @@ int     _test_deny(ConfigFile *conf, ConfigEntry *ce)
 	return errors;	
 }
 
-#ifdef USE_LIBCURL
-static void conf_download_complete(const char *url, const char *file, const char *errorbuf, int cached, void *inc_key)
-{
-	ConfigItem_include *inc;
-	if (!loop.ircd_rehashing)
-		return;
-
-	/*
-	  use inc_key to find the correct include block. This
-	  should be cheaper than using the full URL.
-	 */
-	for (inc = conf_include; inc; inc = (ConfigItem_include *)inc->next)
-	{
-		if ( inc_key != (void *)inc )
-			continue;
-		if (!(inc->flag.type & INCLUDE_REMOTE))
-			continue;
-		if (inc->flag.type & INCLUDE_NOTLOADED)
-			continue;
-		if (stricmp(url, inc->url))
-			continue;
-
-		inc->flag.type &= ~INCLUDE_DLQUEUED;
-		break;
-	}
-	if (!inc)
-	{
-		ircd_log(LOG_ERROR, "Downloaded remote include which matches no include statement.");
-		return;
-	}
-
-	if (!file && !cached)
-		update_remote_include(inc, file, 0, errorbuf); /* DOWNLOAD FAILED */
-	else
-	{
-		char *urlfile = url_getfilename(url);
-		char *file_basename = unreal_getfilename(urlfile);
-		char *tmp = unreal_mktemp("tmp", file_basename);
-		free(urlfile);
-
-		if (cached)
-		{
-			unreal_copyfileex(inc->file, tmp, 1);
-#ifdef REMOTEINC_SPECIALCACHE
-			unreal_copyfileex(inc->file, unreal_mkcache(url), 0);
-#endif
-			update_remote_include(inc, tmp, 0, NULL);
-		}
-		else
-		{
-			/*
-			  copy/hardlink file to another file because our caller will
-			  remove(file).
-			*/
-			unreal_copyfileex(file, tmp, 1);
-			update_remote_include(inc, tmp, 0, NULL);
-#ifdef REMOTEINC_SPECIALCACHE
-			unreal_copyfileex(file, unreal_mkcache(url), 0);
-#endif
-		}
-	}
-	for (inc = conf_include; inc; inc = (ConfigItem_include *)inc->next)
-	{
-		if (inc->flag.type & INCLUDE_DLQUEUED)
-			return;
-	}
-	rehash_internal(loop.rehash_save_cptr, loop.rehash_save_sptr, loop.rehash_save_sig);
-}
-#endif
-
 int     rehash(aClient *cptr, aClient *sptr, int sig)
 {
-#ifdef USE_LIBCURL
-	ConfigItem_include *inc;
-	char found_remote = 0;
-	if (loop.ircd_rehashing)
-	{
-		if (!sig)
-			sendto_one(sptr, ":%s NOTICE %s :A rehash is already in progress",
-				me.name, sptr->name);
-		return 0;
-	}
-
-	loop.ircd_rehashing = 1;
-	loop.rehash_save_cptr = cptr;
-	loop.rehash_save_sptr = sptr;
-	loop.rehash_save_sig = sig;
-	for (inc = conf_include; inc; inc = (ConfigItem_include *)inc->next)
-	{
-		time_t modtime;
-		if (!(inc->flag.type & INCLUDE_REMOTE))
-			continue;
-
-		if (inc->flag.type & INCLUDE_NOTLOADED)
-			continue;
-		found_remote = 1;
-		modtime = unreal_getfilemodtime(inc->file);
-		inc->flag.type |= INCLUDE_DLQUEUED;
-
-		/*
-		  use (void *)inc as the key for finding which
-		  include block conf_download_complete() should use.
-		*/
-		download_file_async(inc->url, modtime, conf_download_complete, (void *)inc);
-	}
-	if (!found_remote)
-		return rehash_internal(cptr, sptr, sig);
-	return 0;
-#else
 	loop.ircd_rehashing = 1;
 	return rehash_internal(cptr, sptr, sig);
-#endif
 }
 
 int	rehash_internal(aClient *cptr, aClient *sptr, int sig)
@@ -9702,115 +9558,6 @@ void	listen_cleanup()
 		close_listeners();
 }
 
-#ifdef USE_LIBCURL
-char *find_remote_include(char *url, char **errorbuf)
-{
-	ConfigItem_include *inc;
-	for (inc = conf_include; inc; inc = (ConfigItem_include *)inc->next)
-	{
-		if (!(inc->flag.type & INCLUDE_NOTLOADED))
-			continue;
-		if (!(inc->flag.type & INCLUDE_REMOTE))
-			continue;
-		if (!stricmp(url, inc->url))
-		{
-			*errorbuf = inc->errorbuf;
-			return inc->file;
-		}
-	}
-	return NULL;
-}
-
-char *find_loaded_remote_include(char *url)
-{
-	ConfigItem_include *inc;
-	for (inc = conf_include; inc; inc = (ConfigItem_include *)inc->next)
-	{
-		if ((inc->flag.type & INCLUDE_NOTLOADED))
-			continue;
-		if (!(inc->flag.type & INCLUDE_REMOTE))
-			continue;
-		if (!stricmp(url, inc->url))
-			return inc->file;
-	}
-	return NULL;
-}	
-
-/**
- * Non-asynchronous remote inclusion to give a user better feedback
- * when first starting his IRCd.
- *
- * The asynchronous friend is rehash() which merely queues remote
- * includes for download using download_file_async().
- */
-int remote_include(ConfigEntry *ce)
-{
-	char *errorbuf = NULL;
-	char *file = find_remote_include(ce->ce_vardata, &errorbuf);
-	int ret;
-	if (!loop.ircd_rehashing || (loop.ircd_rehashing && !file && !errorbuf))
-	{
-		char *error;
-		if (config_verbose > 0)
-			config_status("Downloading %s", ce->ce_vardata);
-		file = download_file(ce->ce_vardata, &error);
-		if (!file)
-		{
-#ifdef REMOTEINC_SPECIALCACHE
-			if (has_cached_version(ce->ce_vardata))
-			{
-				config_warn("%s:%i: include: error downloading '%s': %s -- using cached version instead.",
-					ce->ce_fileptr->cf_filename, ce->ce_varlinenum,
-					ce->ce_vardata, error);
-				file = strdup(unreal_mkcache(ce->ce_vardata));
-				/* Let it pass to load_conf()... */
-			} else {
-#endif
-				config_error("%s:%i: include: error downloading '%s': %s",
-					ce->ce_fileptr->cf_filename, ce->ce_varlinenum,
-					 ce->ce_vardata, error);
-				return -1;
-#ifdef REMOTEINC_SPECIALCACHE
-			}
-#endif
-		}
-		add_remote_include(file, ce->ce_vardata, 0, NULL, ce->ce_fileptr->cf_filename, ce->ce_varlinenum);
-		ret = load_conf(file, ce->ce_vardata);
-		free(file);
-		return ret;
-	}
-	else
-	{
-		if (errorbuf)
-		{
-#ifdef REMOTEINC_SPECIALCACHE
-			if (has_cached_version(ce->ce_vardata))
-			{
-				config_warn("%s:%i: include: error downloading '%s': %s -- using cached version instead.",
-					ce->ce_fileptr->cf_filename, ce->ce_varlinenum,
-					ce->ce_vardata, errorbuf);
-				/* Let it pass to load_conf()... */
-				file = strdup(unreal_mkcache(ce->ce_vardata));
-			} else {
-#endif
-				config_error("%s:%i: include: error downloading '%s': %s",
-					ce->ce_fileptr->cf_filename, ce->ce_varlinenum,
-					ce->ce_vardata, errorbuf);
-				return -1;
-#ifdef REMOTEINC_SPECIALCACHE
-			}
-#endif
-		}
-		if (config_verbose > 0)
-			config_status("Loading %s from download", ce->ce_vardata);
-		add_remote_include(file, ce->ce_vardata, 0, NULL, ce->ce_fileptr->cf_filename, ce->ce_varlinenum);
-		ret = load_conf(file, ce->ce_vardata);
-		return ret;
-	}
-	return 0;
-}
-#endif
-
 /**
  * Add an item to the conf_include list for the specified file.
  *
@@ -9833,58 +9580,6 @@ void add_include(const char *file, const char *included_from, int included_from_
 	AddListItem(inc, conf_include);
 }
 
-#ifdef USE_LIBCURL
-/**
- * Adds a remote include entry to the config_include list.
- *
- * This is to be called whenever the included_from and
- * included_from_line parameters are known. This means that during a
- * rehash when downloads are done asynchronously, you call this with
- * the inclued_from and included_from_line information. After the
- * download is complete and you know there it is stored in the FS,
- * call update_remote_include().
- */
-void add_remote_include(const char *file, const char *url, int flags, const char *errorbuf, const char *included_from, int included_from_line)
-{
-	ConfigItem_include *inc;
-
-	/* we rely on MyMallocEx() zeroing the ConfigItem_include */
-	inc = MyMallocEx(sizeof(ConfigItem_include));
-	if (included_from)
-	{
-		inc->included_from = strdup(included_from);
-		inc->included_from_line = included_from_line;
-	}
-	inc->url = strdup(url);
-
-	update_remote_include(inc, file, INCLUDE_NOTLOADED|INCLUDE_REMOTE|flags, errorbuf);
-	AddListItem(inc, conf_include);
-}
-
-/**
- * Update certain information in a remote include's config_include list entry.
- *
- * @param file the place on disk where the downloaded remote include
- *        may be found
- * @param flags additional flags to set on the config_include entry
- * @param errorbuf non-NULL if there were errors encountered in
- *        downloading. The error will be stored into the config_include
- *        entry.
- */
-void update_remote_include(ConfigItem_include *inc, const char *file, int flags, const char *errorbuf)
-{
-	/*
-	 * file may be NULL when errorbuf is non-NULL and vice-versa.
-	 */
-	if (file)
-		inc->file = strdup(file);
-	inc->flag.type |= flags;
-
-	if (errorbuf)
-		inc->errorbuf = strdup(errorbuf);
-}
-#endif
-
 /**
  * Clean up conf_include after a rehash fails because of a
  * configuration file error.
@@ -9900,15 +9595,6 @@ void unload_notloaded_includes(void)
 		next = (ConfigItem_include *)inc->next;
 		if ((inc->flag.type & INCLUDE_NOTLOADED) || !(inc->flag.type & INCLUDE_USED))
 		{
-#ifdef USE_LIBCURL
-			if (inc->flag.type & INCLUDE_REMOTE)
-			{
-				remove(inc->file);
-				free(inc->url);
-				if (inc->errorbuf)
-					free(inc->errorbuf);
-			}
-#endif
 			free(inc->file);
 			free(inc->included_from);
 			DelListItem(inc, conf_include);
@@ -9930,15 +9616,6 @@ void unload_loaded_includes(void)
 		next = (ConfigItem_include *)inc->next;
 		if (!(inc->flag.type & INCLUDE_NOTLOADED) || !(inc->flag.type & INCLUDE_USED))
 		{
-#ifdef USE_LIBCURL
-			if (inc->flag.type & INCLUDE_REMOTE)
-			{
-				remove(inc->file);
-				free(inc->url);
-				if (inc->errorbuf)
-					free(inc->errorbuf);
-			}
-#endif
 			free(inc->file);
 			free(inc->included_from);
 			DelListItem(inc, conf_include);

autoconf/m4/unreal.m4
@@ -37,124 +37,6 @@ AC_DEFINE([LONG_LONG_RLIM_T], [], [Define if rlim_t is long long])
 fi
 ])
 
-AC_DEFUN([CHECK_LIBCURL],
-[
-	AC_ARG_ENABLE(libcurl,
-	[AC_HELP_STRING([--enable-libcurl=DIR],[enable libcurl (remote include) support])],
-	[enable_curl=$enableval],
-	[enable_curl=no])
-
-	AS_IF([test "x$enable_curl" != "xno"],
-	[
-		dnl sane, default directory for Operating System-managed libcURL
-		dnl (when --enable-libcurl is passed without any arguments). On
-		dnl systems with stuff in /usr/local, /usr/local/bin should already
-		dnl be in PATH. On sane systems, this will invoke the curl-config
-		dnl installed by the package manager.
-		CURLCONFIG="curl-config"
-		AS_IF([test "x$enable_curl" != "xyes"],
-			[CURLCONFIG="$enable_curl/bin/curl-config"])
-
-		AC_MSG_CHECKING([$CURLCONFIG])
-		AS_IF([$CURLCONFIG --version 2>/dev/null >/dev/null],
-			[AC_MSG_RESULT([yes])],
-			[AC_MSG_RESULT([no])
-				AC_MSG_FAILURE([Could not find curl-config, try editing --enable-libcurl])])
-
-		CURLCFLAG="`$CURLCONFIG --cflags`"
-		CURLLIBS="`$CURLCONFIG --libs`"
-
-		dnl This test must be this way because of #3981
-		AS_IF([$CURLCONFIG --features | grep -q -e AsynchDNS],
-			[CURLUSESCARES="1"],
-			[CURLUSESCARES="0"])
-		AS_IF([test "$CURLUSESCARES" = "0"],
-			[AC_MSG_WARN([cURL seems compiled without c-ares support. Your IRCd will possibly stall when REHASHing!])])
-
-		dnl sanity warnings
-		AS_IF([test -z "${CURLLIBS}"],
-			[AC_MSG_WARN([CURLLIBS is empty, that probably means that I could not find $enable_curl/bin/curl-config])])
-
-		dnl Ok this is ugly, basically we need to strip the version of c-ares that curl uses
-		dnl because we want to use our own version (which is hopefully fully binary
-		dnl compatible with the curl one as well).
-		dnl Therefore we need to strip the cares libs in a weird way...
-		dnl If anyone can come up with something better and still portable (no awk!?)
-		dnl then let us know. -- Syzop
-		dnl
-		dnl It is dangerous to mix and match cURL with potentially ABI-incompatible versions of
-		dnl c-ares, just use --with-system-cares.
-		dnl Thus, make sure to use --with-system-cares when using system-cURL. If the user
-		dnl wants bundled c-ares + system libcURL, then we should filter out c-ares
-		dnl flags. _Only_ in that case should we mess with the flags. -- ohnobinki
-
-		AS_IF([test "x$with_system_cares" = "xno" && test "x$HOME/curl" != "x$enable_curl" && test "x/usr/share/unreal-curl" != "x$enable_curl" && test "$CURLUSESCARES" != "0" ],
-		[
-			AC_MSG_ERROR([[
-
-  You have decided to build unrealIRCd with libcURL (remote includes) support.
-  However, you have disabled system-installed c-ares support (--with-system-cares).
-  Because UnrealIRCd will use a bundled copy of c-ares which may be incompatible
-  with the system-installed libcURL, this is a bad idea which may result in error
-  messages looking like:
-
-  	\`\`[error] unrealircd.conf:9: include: error downloading '(http://example.net/ex.conf)': Could not resolve host: example.net (Successful completion)''
-
-  Or UnrealIRCd might even crash.
-
-  Please build UnrealIRCd with --with-system-cares when enabling --enable-libcurl
-]])
-		])
-
-		AS_IF([test "x`echo $CURLLIBS |grep ares`" != x && test "x$with_system_cares" = "xno"],
-		[
-			dnl Attempt one: Linux sed
-			[XCURLLIBS="`echo "$CURLLIBS"|sed -r 's/[^ ]*ares[^ ]*//g' 2>/dev/null`"]
-			AS_IF([test "x$XCURLLIBS" = "x"],
-			[
-				dnl Attempt two: FreeBSD (and others?) sed
-				[XCURLLIBS="`echo "$CURLLIBS"|sed -E 's/[^ ]*ares[^ ]*//g' 2>/dev/null`"]
-				AS_IF([test x"$XCURLLIBS" = x],
-				[
-					AC_MSG_ERROR([sed appears to be broken. It is needed for a remote includes compile hack.])
-				])
-			])
-			CURLLIBS="$XCURLLIBS"
-
-			IRCDLIBS_CURL_CARES="$CARES_LIBS"
-			CFLAGS_CURL_CARES="$CARES_CFLAGS"
-		])
-		
-		dnl Make sure that linking against cURL works rather than letting the user
-		dnl find out after compiling most of his program. ~ohnobinki
-		IRCDLIBS="$IRCDLIBS $CURLLIBS"
-		CFLAGS="$CFLAGS $CURLCFLAG"
-		AC_DEFINE([USE_LIBCURL], [], [Define if you have libcurl installed to get remote includes and MOTD support])
-
-		AC_MSG_CHECKING([curl_easy_init() in $CURLLIBS])
-		LIBS_SAVEDA="$LIBS"
-		CFLAGS_SAVEDA="$CFLAGS"
-
-		LIBS="$IRCDLIBS $IRCDLIBS_CURL_CARES"
-		CFLAGS="$CFLAGS $CFLAGS_CURL_CARES"
-		AC_LINK_IFELSE(
-		    [
-			AC_LANG_PROGRAM(
-			    [[#include <curl/curl.h>]],
-			    [[CURL *curl = curl_easy_init();]])
-			],
-		    [AC_MSG_RESULT([yes])],
-		    [AC_MSG_RESULT([no])
-			AC_MSG_FAILURE([You asked for libcURL (remote includes) support, but it can't be found at $enable_curl])
-		])
-		LIBS="$LIBS_SAVEDA"
-		CFLAGS="$CFLAGS_SAVEDA"
-
-		URL="url.o"
-		AC_SUBST(URL)
-	]) dnl AS_IF(enable_curl) 
-])
-
 dnl the following 2 macros are based on CHECK_SSL by Mark Ethan Trostler <trostler@juniper.net> 
 
 AC_DEFUN([CHECK_SSL],

configure.ac
@@ -671,8 +671,6 @@ dnl use pkg-config for system c-ares:
 PKG_CHECK_MODULES([CARES], libcares >= 1.6.0)
 ])
 
-CHECK_LIBCURL
-
 AC_CONFIG_FILES([Makefile
 	src/modules/Makefile
 	rabbitircd

include/setup.h.in
@@ -377,10 +377,6 @@
    -rc2 for unrealircd-3.2.9-rc2) */
 #undef UNREAL_VERSION_SUFFIX
 
-/* Define if you have libcurl installed to get remote includes and MOTD
-   support */
-#undef USE_LIBCURL
-
 /* Define if you want to allow SSL connections */
 #undef USE_SSL
 

include/struct.h
@@ -137,9 +137,6 @@ typedef struct ListOptions LOpts;
 typedef struct FloodOpt aFloodOpt;
 typedef struct Motd aMotdFile; /* represents a whole MOTD, including remote MOTD support info */
 typedef struct MotdItem aMotdLine; /* one line of a MOTD stored as a linked list */
-#ifdef USE_LIBCURL
-typedef struct MotdDownload aMotdDownload; /* used to coordinate download of a remote MOTD */
-#endif
 
 typedef struct trecord aTrecord;
 typedef struct Command aCommand;
@@ -674,44 +671,10 @@ struct FloodOpt {
 	TS   firstmsg;
 };
 
-#ifdef USE_LIBCURL
-struct Motd;
-struct MotdDownload
-{
-	struct Motd *themotd;
-};
-#endif /* USE_LIBCURL */
-
 struct Motd 
 {
 	struct MotdItem *lines;
 	struct tm last_modified; /* store the last modification time */
-
-#ifdef USE_LIBCURL
-	/*
-	  This pointer is used to communicate with an asynchronous MOTD
-	  download. The problem is that a download may take 10 seconds or
-	  more to complete and, in that time, the IRCd could be rehashed.
-	  This would mean that TLD blocks are reallocated and thus the
-	  aMotd structs would be free()d in the meantime.
-
-	  To prevent such a situation from leading to a segfault, we
-	  introduce this remote control pointer. It works like this:
-	  1. read_motd() is called with a URL. A new MotdDownload is
-	     allocated and the pointer is placed here. This pointer is
-	     also passed to the asynchrnous download handler.
-	  2.a. The download is completed and read_motd_asynch_downloaded()
-	       is called with the same pointer. From this function, this pointer
-	       if free()d. No other code may free() the pointer. Not even free_motd().
-	    OR
-	  2.b. The user rehashes the IRCd before the download is completed.
-	       free_motd() is called, which sets motd_download->themotd to NULL
-	       to signal to read_motd_asynch_downloaded() that it should ignore
-	       the download. read_motd_asynch_downloaded() is eventually called
-	       and frees motd_download.
-	 */
-	struct MotdDownload *motd_download;
-#endif /* USE_LIBCURL */
 };
 
 struct MotdItem {
@@ -1400,10 +1363,6 @@ struct _configitem_include {
 	ConfigItem *prev, *next;
 	ConfigFlag_ban flag;
 	char *file;
-#ifdef USE_LIBCURL
-	char *url;
-	char *errorbuf;
-#endif
 	char *included_from;
 	int included_from_line;
 };

src/s_bsd.c
@@ -97,10 +97,6 @@ extern char *version;
 extern ircstats IRCstats;
 MODVAR TS last_allinuse = 0;
 
-#ifdef USE_LIBCURL
-extern void url_do_transfers_async(void);
-#endif
-
 /*
  * Try and find the correct name to use with getrlimit() for setting the max.
  * number of files allowed to be open by this process.

src/s_serv.c
@@ -40,10 +40,6 @@ static char sccsid[] =
 #include "h.h"
 #include "proto.h"
 #include <string.h>
-#ifdef USE_LIBCURL
-#include "url.h"
-#include <curl/curl.h>
-#endif
 /* for uname(), is POSIX so should be OK... */
 #include <sys/utsname.h>
 extern VOIDSIG s_die();
@@ -64,9 +60,6 @@ aMotdFile smotd;
 
 void read_motd(const char *filename, aMotdFile *motd);
 void do_read_motd(const char *filename, aMotdFile *themotd);
-#ifdef USE_LIBCURL
-void read_motd_asynch_downloaded(const char *url, const char *filename, const char *errorbuf, int cached, aMotdDownload *motd_download);
-#endif
 
 extern aMotdLine *Find_file(char *, short);
 
@@ -184,10 +177,6 @@ CMD_FUNC(m_version)
 #ifdef USE_SSL
 		if (IsAnOper(sptr))
 			sendto_one(sptr, ":%s NOTICE %s :%s", me.name, sptr->name, OPENSSL_VERSION_TEXT);
-#endif
-#ifdef USE_LIBCURL
-		if (IsAnOper(sptr))
-			sendto_one(sptr, ":%s NOTICE %s :%s", me.name, sptr->name, curl_version());
 #endif
 		if (MyClient(sptr))
 normal:
@@ -946,119 +935,12 @@ int short_motd(aClient *sptr)
  */
 void read_motd(const char *filename, aMotdFile *themotd)
 {
-#ifdef USE_LIBCURL
-	time_t modtime;
-	aMotdDownload *motd_download;
-#endif
-
 	/* TODO: if themotd points to a tld's motd,
 	   could a rehash disrupt this pointer?*/
-#ifdef USE_LIBCURL
-	if(themotd->motd_download)
-	{
-		themotd->motd_download->themotd = NULL;
-		/*
-		 * It is not our job to free() motd_download, the
-		 * read_motd_asynch_downloaded() function will do that
-		 * when it sees that ->themod == NULL.
-		 */
-		themotd->motd_download = NULL;
-	}
-
-	/* if filename is NULL, do_read_motd will catch it */
-	if(filename && url_is_valid(filename))
-	{
-		/* prepare our payload for read_motd_asynch_downloaded() */
-		motd_download = MyMallocEx(sizeof(aMotdDownload));
-		if(!motd_download)
-			outofmemory();
-		motd_download->themotd = themotd;
-		themotd->motd_download = motd_download;
-
-#ifdef REMOTEINC_SPECIALCACHE
-		modtime = unreal_getfilemodtime(unreal_mkcache(filename));
-#else
-		modtime = 0;
-#endif
-
-		download_file_async(filename, modtime, (vFP)read_motd_asynch_downloaded, motd_download);
-		return;
-	}
-#endif /* USE_LIBCURL */
-
 	do_read_motd(filename, themotd);
 
 	return;
 }
-
-#ifdef USE_LIBCURL
-/**
-   Callback for download_file_async() called from read_motd()
-   below.
-   @param url the URL curl groked or NULL if the MOTD is stored locally.
-   @param filename the path to the local copy of the MOTD or NULL if either cached=1 or there's an error.
-   @param errorbuf NULL or an errorstring if there was an error while downloading the MOTD.
-   @param cached 0 if the URL was downloaded freshly or 1 if the last download was canceled and the local copy should be used.
- */
-void read_motd_asynch_downloaded(const char *url, const char *filename, const char *errorbuf, int cached, aMotdDownload *motd_download)
-{
-	aMotdFile *themotd;
-
-	themotd = motd_download->themotd;
-	/*
-	  check if the download was soft-canceled. See struct.h's docs on
-	  struct MotdDownload for details.
-	*/
-	if(!themotd)
-	{
-		MyFree(motd_download);
-		return;
-	}
-
-	/* errors -- check for specialcached version if applicable */
-	if(!cached && !filename)
-	{
-#ifdef REMOTEINC_SPECIALCACHE
-		if(has_cached_version(url))
-		{
-			config_warn("Error downloading MOTD file from \"%s\": %s -- using cached version instead.", url, errorbuf);
-			filename = unreal_mkcache(url);
-		} else {
-#endif
-			config_error("Error downloading MOTD file from \"%s\": %s", url, errorbuf);
-
-			/* remove reference to this chunk of memory about to be freed. */
-			motd_download->themotd->motd_download = NULL;
-			MyFree(motd_download);
-			return;
-#ifdef REMOTEINC_SPECIALCACHE
-		}
-#endif
-	}
-
-#ifdef REMOTEINC_SPECIALCACHE
-	/*
-	 * We need to move our newly downloaded file to its cache file
-	 * if it isn't there already.
-	 */
-	if(!cached)
-	{
-		/* create specialcached version for later */
-		unreal_copyfileex(filename, unreal_mkcache(url), 1);
-	} else {
-		/*
-		 * The file is cached. Thus we must look for it at the
-		 * cache location where we placed it earlier.
-		 */
-		filename = unreal_mkcache(url);
-	}
-#endif
-
-	do_read_motd(filename, themotd);
-	MyFree(motd_download);
-}
-#endif /* USE_LIBCURL */
-
 
 /**
    Does the actual reading of the MOTD. To be called only by
@@ -1141,11 +1023,6 @@ void free_motd(aMotdFile *themotd)
 
 	themotd->lines = NULL;
 	memset(&themotd->last_modified, '\0', sizeof(struct tm));
-
-#ifdef USE_LIBCURL
-	/* see struct.h for more information about motd_download */
-	themotd->motd_download = NULL;
-#endif
 }
 
 

commit c6229c88816964e7afd91cf95c54aa9543b5dfad

Fix compile failures introduced in 8000fa002653cd7d21c1834458c87d34926b72c0

src/ircd.c
@@ -60,7 +60,6 @@ Computing Center and Jarkko Oikarinen";
 #endif
 #include "version.h"
 #include "proto.h"
-#endif
 ID_Copyright
     ("(C) 1988 University of Oulu, Computing Center and Jarkko Oikarinen");
 ID_Notes("2.48 3/9/94");
@@ -1109,7 +1108,6 @@ int main(int argc, char *argv[])
 	}
 #endif
 	mkdir("tmp", S_IRUSR|S_IWUSR|S_IXUSR); /* Create the tmp dir, if it doesn't exist */
-#endif
 	/*
 	 * didn't set debuglevel 
 	 */

src/s_conf.c
@@ -3069,7 +3069,6 @@ int	_conf_include(ConfigFile *conf, ConfigEntry *ce)
 		return -1;
 	}
 	chmod(ce->ce_vardata, DEFAULT_PERMISSIONS);
-#endif
 #ifdef GLOBH
 #if defined(__OpenBSD__) && defined(GLOB_LIMIT)
 	glob(ce->ce_vardata, GLOB_NOSORT|GLOB_NOCHECK|GLOB_LIMIT, NULL, &files);