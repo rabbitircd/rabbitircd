rabbitircd/rabbitircd

commit 3b33d98405779452d63fc943c7f8d18d36cf94e6

Start removing badwords from the core (feature #3)

include/h.h
@@ -403,7 +403,7 @@ extern MODVAR long UMODE_VICTIM;    /* 0x8000000	 Intentional Victim */
 extern MODVAR long UMODE_DEAF;      /* 0x10000000       Deaf */
 extern MODVAR long UMODE_HIDEOPER;  /* 0x20000000	 Hide oper mode */
 extern MODVAR long UMODE_SETHOST;   /* 0x40000000	 used sethost */
-extern MODVAR long UMODE_STRIPBADWORDS; /* 0x80000000	 */
+//UNUSED 0x80000000 was STRIPBADWORDS
 extern MODVAR long UMODE_HIDEWHOIS; /* hides channels in /whois */
 extern MODVAR long UMODE_NOCTCP;    /* blocks all ctcp (except dcc and action) */
 extern MODVAR long UMODE_HIDLE;     /* hides oper idle times */
@@ -426,9 +426,6 @@ extern MODVAR long SNO_OPER;
 
 /* Extended chanmodes... */
 extern MODVAR Cmode_t EXTMODE_NONOTICE;
-#ifdef STRIPBADWORDS
-extern MODVAR Cmode_t EXTMODE_STRIPBADWORDS;
-#endif
 extern MODVAR Cmode_t EXTMODE_JOINTHROTTLE;
 
 #ifndef HAVE_STRLCPY

src/s_conf.c
@@ -37,9 +37,6 @@
 #ifdef GLOBH
 #include <glob.h>
 #endif
-#ifdef STRIPBADWORDS
-#include "badwords.h"
-#endif
 #include "h.h"
 #include "inet.h"
 #include "proto.h"
@@ -85,9 +82,6 @@ static int	_conf_vhost		(ConfigFile *conf, ConfigEntry *ce);
 static int	_conf_link		(ConfigFile *conf, ConfigEntry *ce);
 static int	_conf_ban		(ConfigFile *conf, ConfigEntry *ce);
 static int	_conf_set		(ConfigFile *conf, ConfigEntry *ce);
-#ifdef STRIPBADWORDS
-static int	_conf_badword		(ConfigFile *conf, ConfigEntry *ce);
-#endif
 static int	_conf_deny		(ConfigFile *conf, ConfigEntry *ce);
 static int	_conf_deny_dcc		(ConfigFile *conf, ConfigEntry *ce);
 static int	_conf_deny_link		(ConfigFile *conf, ConfigEntry *ce);
@@ -123,9 +117,6 @@ static int	_test_vhost		(ConfigFile *conf, ConfigEntry *ce);
 static int	_test_link		(ConfigFile *conf, ConfigEntry *ce);
 static int	_test_ban		(ConfigFile *conf, ConfigEntry *ce);
 static int	_test_set		(ConfigFile *conf, ConfigEntry *ce);
-#ifdef STRIPBADWORDS
-static int	_test_badword		(ConfigFile *conf, ConfigEntry *ce);
-#endif
 static int	_test_deny		(ConfigFile *conf, ConfigEntry *ce);
 static int	_test_allow_channel	(ConfigFile *conf, ConfigEntry *ce);
 static int	_test_allow_dcc		(ConfigFile *conf, ConfigEntry *ce);
@@ -142,9 +133,6 @@ static ConfigCommand _ConfigCommands[] = {
 	{ "admin", 		_conf_admin,		_test_admin 	},
 	{ "alias",		_conf_alias,		_test_alias	},
 	{ "allow",		_conf_allow,		_test_allow	},
-#ifdef STRIPBADWORDS
-	{ "badword",		_conf_badword,		_test_badword	},
-#endif
 	{ "ban", 		_conf_ban,		_test_ban	},
 	{ "cgiirc", 	_conf_cgiirc,	_test_cgiirc	},
 	{ "class", 		_conf_class,		_test_class	},
@@ -370,11 +358,6 @@ ConfigItem_log		*conf_log = NULL;
 ConfigItem_alias	*conf_alias = NULL;
 ConfigItem_include	*conf_include = NULL;
 ConfigItem_help		*conf_help = NULL;
-#ifdef STRIPBADWORDS
-ConfigItem_badword	*conf_badword_channel = NULL;
-ConfigItem_badword      *conf_badword_message = NULL;
-ConfigItem_badword	*conf_badword_quit = NULL;
-#endif
 ConfigItem_offchans	*conf_offchans = NULL;
 
 aConfiguration		iConf;
@@ -2138,38 +2121,6 @@ void	config_rehash()
 		MyFree(vhost_ptr);
 	}
 
-#ifdef STRIPBADWORDS
-	for (badword_ptr = conf_badword_channel; badword_ptr;
-		badword_ptr = (ConfigItem_badword *) next) {
-		next = (ListStruct *)badword_ptr->next;
-		ircfree(badword_ptr->word);
-		if (badword_ptr->replace)
-			ircfree(badword_ptr->replace);
-		regfree(&badword_ptr->expr);
-		DelListItem(badword_ptr, conf_badword_channel);
-		MyFree(badword_ptr);
-	}
-	for (badword_ptr = conf_badword_message; badword_ptr;
-		badword_ptr = (ConfigItem_badword *) next) {
-		next = (ListStruct *)badword_ptr->next;
-		ircfree(badword_ptr->word);
-		if (badword_ptr->replace)
-			ircfree(badword_ptr->replace);
-		regfree(&badword_ptr->expr);
-		DelListItem(badword_ptr, conf_badword_message);
-		MyFree(badword_ptr);
-	}
-	for (badword_ptr = conf_badword_quit; badword_ptr;
-		badword_ptr = (ConfigItem_badword *) next) {
-		next = (ListStruct *)badword_ptr->next;
-		ircfree(badword_ptr->word);
-		if (badword_ptr->replace)
-			ircfree(badword_ptr->replace);
-		regfree(&badword_ptr->expr);
-		DelListItem(badword_ptr, conf_badword_quit);
-		MyFree(badword_ptr);
-	}
-#endif
 	/* Clean up local spamfilter entries... */
 	for (tk = tklines[tkl_hash('f')]; tk; tk = tk_next)
 	{
@@ -5686,212 +5637,6 @@ int	_test_vhost(ConfigFile *conf, ConfigEntry *ce)
 	}
 	return errors;
 }
-
-#ifdef STRIPBADWORDS
-
-static ConfigItem_badword *copy_badword_struct(ConfigItem_badword *ca, int regex, int regflags)
-{
-	ConfigItem_badword *x = MyMalloc(sizeof(ConfigItem_badword));
-	memcpy(x, ca, sizeof(ConfigItem_badword));
-	x->word = strdup(ca->word);
-	if (ca->replace)
-		x->replace = strdup(ca->replace);
-	if (regex) 
-	{
-		memset(&x->expr, 0, sizeof(regex_t));
-		regcomp(&x->expr, x->word, regflags);
-	}
-	return x;
-}
-
-int     _conf_badword(ConfigFile *conf, ConfigEntry *ce)
-{
-	ConfigEntry *cep, *word = NULL;
-	ConfigItem_badword *ca;
-	char *tmp;
-	short regex = 0;
-	int regflags = 0;
-	int ast_l = 0, ast_r = 0;
-
-	ca = MyMallocEx(sizeof(ConfigItem_badword));
-	ca->action = BADWORD_REPLACE;
-	regflags = REG_ICASE|REG_EXTENDED;
-
-	for (cep = ce->ce_entries; cep; cep = cep->ce_next)
-	{
-		if (!strcmp(cep->ce_varname, "action"))
-		{
-			if (!strcmp(cep->ce_vardata, "block"))
-			{
-				ca->action = BADWORD_BLOCK;
-				/* If it is set to just block, then we don't need to worry about
-				 * replacements 
-				 */
-				regflags |= REG_NOSUB;
-			}
-		}
-		else if (!strcmp(cep->ce_varname, "replace"))
-		{
-			ircstrdup(ca->replace, cep->ce_vardata);
-		}
-		else if (!strcmp(cep->ce_varname, "word"))
-			word = cep;
-	}
-	/* The fast badwords routine can do: "blah" "*blah" "blah*" and "*blah*",
-	 * in all other cases use regex.
-	 */
-	for (tmp = word->ce_vardata; *tmp; tmp++) {
-		if (!isalnum(*tmp) && !(*tmp >= 128)) {
-			if ((word->ce_vardata == tmp) && (*tmp == '*')) {
-				ast_l = 1; /* Asterisk at the left */
-				continue;
-			}
-			if ((*(tmp + 1) == '\0') && (*tmp == '*')) {
-				ast_r = 1; /* Asterisk at the right */
-				continue;
-			}
-			regex = 1;
-			break;
-		}
-	}
-	if (regex)
-	{
-		ca->type = BADW_TYPE_REGEX;
-		ircstrdup(ca->word, word->ce_vardata);
-		regcomp(&ca->expr, ca->word, regflags);
-	}
-	else
-	{
-		char *tmpw;
-		ca->type = BADW_TYPE_FAST;
-		ca->word = tmpw = MyMalloc(strlen(word->ce_vardata) - ast_l - ast_r + 1);
-		/* Copy except for asterisks */
-		for (tmp = word->ce_vardata; *tmp; tmp++)
-			if (*tmp != '*')
-				*tmpw++ = *tmp;
-		*tmpw = '\0';
-		if (ast_l)
-			ca->type |= BADW_TYPE_FAST_L;
-		if (ast_r)
-			ca->type |= BADW_TYPE_FAST_R;
-	}
-	if (!strcmp(ce->ce_vardata, "channel"))
-		AddListItem(ca, conf_badword_channel);
-	else if (!strcmp(ce->ce_vardata, "message"))
-		AddListItem(ca, conf_badword_message);
-	else if (!strcmp(ce->ce_vardata, "quit"))
-		AddListItem(ca, conf_badword_quit);
-	else if (!strcmp(ce->ce_vardata, "all"))
-	{
-		AddListItem(ca, conf_badword_channel);
-		AddListItem(copy_badword_struct(ca,regex,regflags), conf_badword_message);
-		AddListItem(copy_badword_struct(ca,regex,regflags), conf_badword_quit);
-	}
-	return 1;
-}
-
-int _test_badword(ConfigFile *conf, ConfigEntry *ce) 
-{ 
-	int errors = 0;
-	ConfigEntry *cep;
-	char has_word = 0, has_replace = 0, has_action = 0, action = 'r';
-
-	if (!ce->ce_vardata)
-	{
-		config_error("%s:%i: badword without type",
-			ce->ce_fileptr->cf_filename, ce->ce_varlinenum);
-		return 1;
-	}
-	else if (strcmp(ce->ce_vardata, "channel") && strcmp(ce->ce_vardata, "message") && 
-	         strcmp(ce->ce_vardata, "quit") && strcmp(ce->ce_vardata, "all")) {
-			config_error("%s:%i: badword with unknown type",
-				ce->ce_fileptr->cf_filename, ce->ce_varlinenum);
-		return 1;
-	}
-	for (cep = ce->ce_entries; cep; cep = cep->ce_next)
-	{
-		if (config_is_blankorempty(cep, "badword"))
-		{
-			errors++;
-			continue;
-		}
-		if (!strcmp(cep->ce_varname, "word"))
-		{
-			char *errbuf;
-			if (has_word)
-			{
-				config_warn_duplicate(cep->ce_fileptr->cf_filename, 
-					cep->ce_varlinenum, "badword::word");
-				continue;
-			}
-			has_word = 1;
-			if ((errbuf = unreal_checkregex(cep->ce_vardata,1,1)))
-			{
-				config_error("%s:%i: badword::%s contains an invalid regex: %s",
-					cep->ce_fileptr->cf_filename,
-					cep->ce_varlinenum,
-					cep->ce_varname, errbuf);
-				errors++;
-			}
-		}
-		else if (!strcmp(cep->ce_varname, "replace"))
-		{
-			if (has_replace)
-			{
-				config_warn_duplicate(cep->ce_fileptr->cf_filename, 
-					cep->ce_varlinenum, "badword::replace");
-				continue;
-			}
-			has_replace = 1;
-		}
-		else if (!strcmp(cep->ce_varname, "action"))
-		{
-			if (has_action)
-			{
-				config_warn_duplicate(cep->ce_fileptr->cf_filename, 
-					cep->ce_varlinenum, "badword::action");
-				continue;
-			}
-			has_action = 1;
-			if (!strcmp(cep->ce_vardata, "replace"))
-				action = 'r';
-			else if (!strcmp(cep->ce_vardata, "block"))
-				action = 'b';
-			else
-			{
-				config_error("%s:%d: Unknown badword::action '%s'",
-					cep->ce_fileptr->cf_filename, cep->ce_varlinenum,
-					cep->ce_vardata);
-				errors++;
-			}
-				
-		}
-		else
-		{
-			config_error_unknown(cep->ce_fileptr->cf_filename, cep->ce_varlinenum,
-				"badword", cep->ce_varname);
-			errors++;
-		}
-	}
-
-	if (!has_word)
-	{
-		config_error_missing(ce->ce_fileptr->cf_filename, ce->ce_varlinenum,
-			"badword::word");
-		errors++;
-	}
-	if (has_action)
-	{
-		if (has_replace && action == 'b')
-		{
-			config_error("%s:%i: badword::action is block but badword::replace exists",
-				ce->ce_fileptr->cf_filename, ce->ce_varlinenum);
-			errors++;
-		}
-	}
-	return errors; 
-}
-#endif
 
 int _conf_spamfilter(ConfigFile *conf, ConfigEntry *ce)
 {

doc/technical/serverprotocol.html
@@ -134,7 +134,7 @@
 			<li>n : NOSPOOF (pingcookies) is enabled (#define NOSPOOF or answer "Yes" to relevant ./Config prompt).</li>
 			<li>Y : Syslog logging enabled.</li>
 			<li>6 : Server has IPv6 support (#define INET6 or answer "yes" to relevant ./Config prompt).</li>
-			<li>X : Server has badword stripping (user and channel modes +G) (#define STRIPBADWORDS).</li>
+			<li>X : Obsolete</li>
 			<li>P : Server is using poll().</li>
 			<li>e : Server has SSL Support (#define USE_SSL or answer "yes" (and have ssl libraries installed) to relevant ./Config prompt).</li>
 			<li>O : Server has OperOverride enabled (#undef NO_OPEROVERRIDE or answer "no" to relevant ./Config prompt).</li>

extras/channeldumper.c
@@ -35,9 +35,6 @@
 #include <fcntl.h>
 #include "h.h"
 #include "proto.h"
-#ifdef STRIPBADWORDS
-#include "badwords.h"
-#endif
 
 #ifndef DYNAMIC_LINKING
 ModuleHeader channeldumper_Header

extras/defizzer.c
@@ -21,9 +21,6 @@
 #include <string.h>
 #include <fcntl.h>
 #include "h.h"
-#ifdef STRIPBADWORDS
-#include "badwords.h"
-#endif
 DLLFUNC int h_defizzer_connect(aClient *sptr);
 
 static Hook *LocConnect = NULL;

include/config.h
@@ -120,16 +120,6 @@
 #define REMOTE_REHASH
 
 /*
-  Stripbadwords patch
-*/
-#define STRIPBADWORDS
-
-/*
- * Always strip badwords in channels? (channel does not have to be +G)
-*/
-#undef STRIPBADWORDS_CHAN_ALWAYS
-
-/*
  * No spoof code
  *
  * This enables the spoof protection.

include/modules.h
@@ -691,9 +691,9 @@ int CallCmdoverride(Cmdoverride *ovr, aClient *cptr, aClient *sptr, int parc, ch
 #define EFUNC_DOSPAMFILTER_VIRUSCHAN		25
 #define EFUNC_FIND_TKLINE_MATCH_ZAP_EX		26
 #define EFUNC_SEND_LIST						27
-#define EFUNC_STRIPBADWORDS_CHANNEL			28
-#define EFUNC_STRIPBADWORDS_MESSAGE			29
-#define EFUNC_STRIPBADWORDS_QUIT			30
+#define EFUNC_STRIPBADWORDS_CHANNEL                     28
+#define EFUNC_STRIPBADWORDS_MESSAGE                     29
+#define EFUNC_STRIPBADWORDS_QUIT                        30
 #define EFUNC_STRIPCOLORS					31
 #define EFUNC_STRIPCONTROLCODES				32
 #define EFUNC_SPAMFILTER_BUILD_USER_STRING	33

include/struct.h
@@ -103,7 +103,6 @@ typedef struct _configitem_except ConfigItem_except;
 typedef struct _configitem_link	ConfigItem_link;
 typedef struct _configitem_cgiirc ConfigItem_cgiirc;
 typedef struct _configitem_ban ConfigItem_ban;
-typedef struct _configitem_badword ConfigItem_badword;
 typedef struct _configitem_deny_dcc ConfigItem_deny_dcc;
 typedef struct _configitem_deny_link ConfigItem_deny_link;
 typedef struct _configitem_deny_channel ConfigItem_deny_channel;
@@ -351,10 +350,6 @@ typedef unsigned int u_int32_t;	/* XXX Hope this works! */
 #define IsKix(x)		((x)->umodes & UMODE_KIX)
 #define IsHelpOp(x)		((x)->umodes & UMODE_HELPOP)
 #define IsAdmin(x)		((x)->umodes & UMODE_ADMIN)
-
-#ifdef STRIPBADWORDS
-#define IsFilteringWords(x)	((x)->umodes & UMODE_STRIPBADWORDS)
-#endif
 #define IsNetAdmin(x)		((x)->umodes & UMODE_NETADMIN)
 #define IsCoAdmin(x)		((x)->umodes & UMODE_COADMIN)
 #define IsSAdmin(x)		((x)->umodes & UMODE_SADMIN)
@@ -1227,24 +1222,6 @@ struct _iplist {
 	IPList *prev, *next;
 	char *mask;
 /*	struct irc_netmask  *netmask; */
-};
-
-#define BADW_TYPE_INVALID 0x0
-#define BADW_TYPE_FAST    0x1
-#define BADW_TYPE_FAST_L  0x2
-#define BADW_TYPE_FAST_R  0x4
-#define BADW_TYPE_REGEX   0x8
-
-#define BADWORD_REPLACE 1
-#define BADWORD_BLOCK 2
-
-struct _configitem_badword {
-	ConfigItem      *prev, *next;
-	ConfigFlag	flag;
-	char		*word, *replace;
-	unsigned short	type;
-	char		action;
-	regex_t 	expr;
 };
 
 struct _configitem_deny_dcc {

commit 40202537593aeb21dce42ce699bcc5de1d6bc351

Fix compile failure introduced in 3b33d98405779452d63fc943c7f8d18d36cf94e6 and remove more badwords crap from core (feature #3)

include/h.h
@@ -68,9 +68,6 @@ extern MODVAR ConfigItem_except	*conf_except;
 extern MODVAR ConfigItem_vhost		*conf_vhost;
 extern MODVAR ConfigItem_link		*conf_link;
 extern MODVAR ConfigItem_ban		*conf_ban;
-extern MODVAR ConfigItem_badword	*conf_badword_channel;
-extern MODVAR ConfigItem_badword       *conf_badword_message;
-extern MODVAR ConfigItem_badword	*conf_badword_quit;
 extern MODVAR ConfigItem_deny_dcc	*conf_deny_dcc;
 extern MODVAR ConfigItem_deny_channel  *conf_deny_channel;
 extern MODVAR ConfigItem_deny_link	*conf_deny_link;
@@ -673,9 +670,6 @@ extern MODVAR int (*place_host_ban)(aClient *sptr, int action, char *reason, lon
 extern MODVAR int (*dospamfilter)(aClient *sptr, char *str_in, int type, char *target, int flags, aTKline **rettk);
 extern MODVAR int (*dospamfilter_viruschan)(aClient *sptr, aTKline *tk, int type);
 extern MODVAR void (*send_list)(aClient *cptr, int numsend);
-extern MODVAR char *(*stripbadwords_channel)(char *str, int *blocked);
-extern MODVAR char *(*stripbadwords_message)(char *str, int *blocked);
-extern MODVAR char *(*stripbadwords_quit)(char *str, int *blocked);
 extern MODVAR unsigned char *(*StripColors)(unsigned char *text);
 extern MODVAR const char *(*StripControlCodes)(unsigned char *text);
 extern MODVAR void (*spamfilter_build_user_string)(char *buf, char *nick, aClient *acptr);

src/channel.c
@@ -756,8 +756,28 @@ int  is_chanprot(aClient *cptr, aChannel *chptr)
 #define CANNOT_SEND_BAN 4
 #define CANNOT_SEND_NOCTCP 5
 #define CANNOT_SEND_MODREG 6
-#define CANNOT_SEND_SWEAR 7 /* This isn't actually used here */
-#define CANNOT_SEND_NOTICE 8 
+#define CANNOT_SEND_NOTICE 7 
+
+#define ERR_CANTSEND_VOICE "You need voice (+v)"
+#define ERR_CANTSEND_EXTERNAL "You must be joined to this channel"
+#define ERR_CANTSEND_COLOUR "Colour is not permitted in this channel"
+#define ERR_CANTSEND_BANNED "You are banned from this channel"
+#define ERR_CANTSEND_CTCP "CTCPs are not permitted in this channel"
+#define ERR_CANTSEND_REGISTERED "You must have a registered nick (+r) to talk on this channel"
+#define ERR_CANTSEND_NOTICE "NOTICEs are not permitted in this channel"
+
+inline const char* cantsend_strerror(uint32_t reason) {
+    switch (reason) {
+    case 1: return "You need voice (+v)";
+    case 2: return "You must be joined to this channel";
+    case 3: return "Colour is not permitted in this channel";
+    case 4: return "You are banned from this channel";
+    case 5: return "CTCPs are not permitted in this channel";
+    case 6: return "You must have a registered nick (+r) to talk on this channel";
+    case 7: return "NOTICEs are not permitted in this channel";
+    default: return "No reason given";
+    }
+}
 
 int  can_send(aClient *cptr, aChannel *chptr, char *msgtext, int notice)
 {

src/modules/m_message.c
@@ -33,15 +33,8 @@
 #include <fcntl.h>
 #include "h.h"
 #include "proto.h"
-#ifdef STRIPBADWORDS
-#include "badwords.h"
-#endif
-#include "badwords.h"
 
 int _is_silenced(aClient *, aClient *);
-char *_stripbadwords_channel(char *str, int *blocked);
-char *_stripbadwords_message(char *str, int *blocked);
-char *_stripbadwords_quit(char *str, int *blocked);
 char *_StripColors(unsigned char *text);
 char *_StripControlCodes(unsigned char *text);
 
@@ -67,9 +60,6 @@ ModuleHeader MOD_HEADER(m_message)
 DLLFUNC int MOD_TEST(m_message)(ModuleInfo *modinfo)
 {
 	MARK_AS_OFFICIAL_MODULE(modinfo);
-	EfunctionAddPChar(modinfo->handle, EFUNC_STRIPBADWORDS_CHANNEL, _stripbadwords_channel);
-	EfunctionAddPChar(modinfo->handle, EFUNC_STRIPBADWORDS_MESSAGE, _stripbadwords_message);
-	EfunctionAddPChar(modinfo->handle, EFUNC_STRIPBADWORDS_QUIT, _stripbadwords_quit);
 	EfunctionAddPChar(modinfo->handle, EFUNC_STRIPCOLORS, _StripColors);
 	EfunctionAddPChar(modinfo->handle, EFUNC_STRIPCONTROLCODES, _StripControlCodes);
 	EfunctionAdd(modinfo->handle, EFUNC_IS_SILENCED, _is_silenced);
@@ -158,9 +148,6 @@ int ret;
 
 	if (!is_silenced(sptr, acptr))
 	{
-#ifdef STRIPBADWORDS
-		int blocked = 0;
-#endif
 		Hook *tmphook;
 		
 		if (!notice && MyConnect(sptr) &&
@@ -168,20 +155,6 @@ int ret;
 			sendto_one(sptr, rpl_str(RPL_AWAY),
 			    me.name, sptr->name, acptr->name,
 			    acptr->user->away);
-
-#ifdef STRIPBADWORDS
-		if (MyClient(sptr) && !IsULine(acptr) && IsFilteringWords(acptr))
-		{
-			*text = stripbadwords_message(*text, &blocked);
-			if (blocked)
-			{
-				if (!notice && MyClient(sptr))
-					sendto_one(sptr, rpl_str(ERR_NOSWEAR),
-						me.name, sptr->name, acptr->name);
-				return CANPRIVMSG_CONTINUE;
-			}
-		}
-#endif
 
 		if (MyClient(sptr))
 		{
@@ -218,6 +191,7 @@ int ret;
 ** rev argv 6/91
 **
 */
+
 static int recursive_webtv = 0;
 DLLFUNC int m_message(aClient *cptr, aClient *sptr, int parc, char *parv[], int notice)
 {
@@ -229,21 +203,6 @@ DLLFUNC int m_message(aClient *cptr, aClient *sptr, int parc, char *parv[], int 
 	int  prefix = 0;
 	char pfixchan[CHANNELLEN + 4];
 	int ret;
-
-	/*
-	 * Reasons why someone can't send to a channel
-	 */
-	static char *err_cantsend[] = {
-		"You need voice (+v)",
-		"No external channel messages",
-		"Color is not permitted in this channel",
-		"You are banned",
-		"CTCPs are not permitted in this channel",
-		"You must have a registered nick (+r) to talk on this channel",
-		"Swearing is not permitted in this channel",
-		"NOTICEs are not permitted in this channel",
-		NULL
-	};
 
 	if (IsHandshake(sptr))
 		return 0;
@@ -422,13 +381,9 @@ DLLFUNC int m_message(aClient *cptr, aClient *sptr, int parc, char *parv[], int 
 				continue;
 			}
 			
-			cansend =
-			    !IsULine(sptr) ? can_send(sptr, chptr, parv[2], notice) : 0;
+			cansend = !IsULine(sptr) ? can_send(sptr, chptr, parv[2], notice) : 0;
 			if (!cansend)
 			{
-#ifdef STRIPBADWORDS
-				int blocked = 0;
-#endif
 				Hook *tmphook;
 
 				if (chptr->mode.floodprot && chptr->mode.floodprot->l[FLD_TEXT])
@@ -439,35 +394,6 @@ DLLFUNC int m_message(aClient *cptr, aClient *sptr, int parc, char *parv[], int 
 				text = parv[2];
 				if (MyClient(sptr) && (chptr->mode.mode & MODE_STRIP))
 					text = StripColors(parv[2]);
-#ifdef STRIPBADWORDS
-				if (MyClient(sptr))
-				{
- #ifdef STRIPBADWORDS_CHAN_ALWAYS
-					text = stripbadwords_channel(text,& blocked);
-					if (blocked)
-					{
-						if (!notice)
-							sendto_one(sptr, err_str(ERR_CANNOTSENDTOCHAN),
-							    me.name, parv[0], chptr->chname,
-							    err_cantsend[6], p2);
-						continue;
-					}
- #else
-					if (chptr->mode.extmode & EXTMODE_STRIPBADWORDS)
-					{
-						text = stripbadwords_channel(text, &blocked);
-						if (blocked)
-						{
-							if (!notice)
-								sendto_one(sptr, err_str(ERR_CANNOTSENDTOCHAN),
-								    me.name, parv[0], chptr->chname,
-								    err_cantsend[6], p2);
-							continue;
-						}
-					}
- #endif
-				}
-#endif
 
 				if (MyClient(sptr))
 				{
@@ -514,7 +440,7 @@ DLLFUNC int m_message(aClient *cptr, aClient *sptr, int parc, char *parv[], int 
 				if (!notice || (cansend == 8)) /* privmsg or 'cannot send notice'... */
 					sendto_one(sptr, err_str(ERR_CANNOTSENDTOCHAN),
 					    me.name, parv[0], chptr->chname,
-					    err_cantsend[cansend - 1], p2);
+					    cantsend_strerror(cansend), p2);
 			}
 			continue;
 		}
@@ -881,277 +807,6 @@ int size_string;
 
 	return 1; /* allowed */
 }
-
-/* This was modified a bit in order to use newconf. The loading functions
- * have been trashed and integrated into the config parser. The striping
- * function now only uses REPLACEWORD if no word is specifically defined
- * for the word found. Also the freeing function has been ditched. -- codemastr
- */
-
-/*
- * our own strcasestr implementation because strcasestr is often not
- * available or is not working correctly (??).
- */
-char *our_strcasestr(char *haystack, char *needle) {
-int i;
-int nlength = strlen (needle);
-int hlength = strlen (haystack);
-
-	if (nlength > hlength) return NULL;
-	if (hlength <= 0) return NULL;
-	if (nlength <= 0) return haystack;
-	for (i = 0; i <= (hlength - nlength); i++) {
-		if (strncasecmp (haystack + i, needle, nlength) == 0)
-			return haystack + i;
-	}
-  return NULL; /* not found */
-}
-inline int fast_badword_match(ConfigItem_badword *badword, char *line)
-{
- 	char *p;
-	int bwlen = strlen(badword->word);
-	if ((badword->type & BADW_TYPE_FAST_L) && (badword->type & BADW_TYPE_FAST_R))
-		return (our_strcasestr(line, badword->word) ? 1 : 0);
-
-	p = line;
-	while((p = our_strcasestr(p, badword->word)))
-	{
-		if (!(badword->type & BADW_TYPE_FAST_L))
-		{
-			if ((p != line) && !iswseperator(*(p - 1))) /* aaBLA but no *BLA */
-				goto next;
-		}
-		if (!(badword->type & BADW_TYPE_FAST_R))
-		{
-			if (!iswseperator(*(p + bwlen)))  /* BLAaa but no BLA* */
-				goto next;
-		}
-		/* Looks like it matched */
-		return 1;
-next:
-		p += bwlen;
-	}
-	return 0;
-}
-/* fast_badword_replace:
- * a fast replace routine written by Syzop used for replacing badwords.
- * searches in line for huntw and replaces it with replacew,
- * buf is used for the result and max is sizeof(buf).
- * (Internal assumptions: max > 0 AND max > strlen(line)+1)
- */
-inline int fast_badword_replace(ConfigItem_badword *badword, char *line, char *buf, int max)
-{
-/* Some aliases ;P */
-char *replacew = badword->replace ? badword->replace : REPLACEWORD;
-char *pold = line, *pnew = buf; /* Pointers to old string and new string */
-char *poldx = line;
-int replacen = -1; /* Only calculated if needed. w00t! saves us a few nanosecs? lol */
-int searchn = -1;
-char *startw, *endw;
-char *c_eol = buf + max - 1; /* Cached end of (new) line */
-int run = 1;
-int cleaned = 0;
-
-	Debug((DEBUG_NOTICE, "replacing %s -> %s in '%s'", badword->word, replacew, line));
-
-	while(run) {
-		pold = our_strcasestr(pold, badword->word);
-		if (!pold)
-			break;
-		if (replacen == -1)
-			replacen = strlen(replacew);
-		if (searchn == -1)
-			searchn = strlen(badword->word);
-		/* Hunt for start of word */
- 		if (pold > line) {
-			for (startw = pold; (!iswseperator(*startw) && (startw != line)); startw--);
-			if (iswseperator(*startw))
-				startw++; /* Don't point at the space/seperator but at the word! */
-		} else {
-			startw = pold;
-		}
-
-		if (!(badword->type & BADW_TYPE_FAST_L) && (pold != startw)) {
-			/* not matched */
-			pold++;
-			continue;
-		}
-
-		/* Hunt for end of word */
-		for (endw = pold; ((*endw != '\0') && (!iswseperator(*endw))); endw++);
-
-		if (!(badword->type & BADW_TYPE_FAST_R) && (pold+searchn != endw)) {
-			/* not matched */
-			pold++;
-			continue;
-		}
-
-		cleaned = 1; /* still too soon? Syzop/20050227 */
-
-		/* Do we have any not-copied-yet data? */
-		if (poldx != startw) {
-			int tmp_n = startw - poldx;
-			if (pnew + tmp_n >= c_eol) {
-				/* Partial copy and return... */
-				memcpy(pnew, poldx, c_eol - pnew);
-				*c_eol = '\0';
-				return 1;
-			}
-
-			memcpy(pnew, poldx, tmp_n);
-			pnew += tmp_n;
-		}
-		/* Now update the word in buf (pnew is now something like startw-in-new-buffer */
-
-		if (replacen) {
-			if ((pnew + replacen) >= c_eol) {
-				/* Partial copy and return... */
-				memcpy(pnew, replacew, c_eol - pnew);
-				*c_eol = '\0';
-				return 1;
-			}
-			memcpy(pnew, replacew, replacen);
-			pnew += replacen;
-		}
-		poldx = pold = endw;
-	}
-	/* Copy the last part */
-	if (*poldx) {
-		strncpy(pnew, poldx, c_eol - pnew);
-		*(c_eol) = '\0';
-	} else {
-		*pnew = '\0';
-	}
-	return cleaned;
-}
-
-/*
- * Returns a string, which has been filtered by the words loaded via
- * the loadbadwords() function.  It's primary use is to filter swearing
- * in both private and public messages
- */
-
-char *stripbadwords(char *str, ConfigItem_badword *start_bw, int *blocked)
-{
-	regmatch_t pmatch[MAX_MATCH];
-	static char cleanstr[4096];
-	char buf[4096];
-	char *ptr;
-	int  matchlen, m, stringlen, cleaned;
-	ConfigItem_badword *this_word;
-	
-	*blocked = 0;
-
-	if (!start_bw)
-		return str;
-
-	/*
-	 * work on a copy
-	 */
-	stringlen = strlcpy(cleanstr, StripControlCodes(str), sizeof cleanstr);
-	memset(&pmatch, 0, sizeof pmatch);
-	matchlen = 0;
-	buf[0] = '\0';
-	cleaned = 0;
-
-	for (this_word = start_bw; this_word; this_word = (ConfigItem_badword *)this_word->next)
-	{
-		if (this_word->type & BADW_TYPE_FAST)
-		{
-			if (this_word->action == BADWORD_BLOCK)
-			{
-				if (fast_badword_match(this_word, cleanstr))
-				{
-					*blocked = 1;
-					return NULL;
-				}
-			}
-			else
-			{
-				int n;
-				/* fast_badword_replace() does size checking so we can use 512 here instead of 4096 */
-				n = fast_badword_replace(this_word, cleanstr, buf, 512);
-				if (!cleaned && n)
-					cleaned = n;
-				strcpy(cleanstr, buf);
-				memset(buf, 0, sizeof(buf)); /* regexp likes this somehow */
-			}
-		} else
-		if (this_word->type & BADW_TYPE_REGEX)
-		{
-			if (this_word->action == BADWORD_BLOCK)
-			{
-				if (!regexec(&this_word->expr, cleanstr, 0, NULL, 0))
-				{
-					*blocked = 1;
-					return NULL;
-				}
-			}
-			else
-			{
-				ptr = cleanstr; /* set pointer to start of string */
-				while (regexec(&this_word->expr, ptr, MAX_MATCH, pmatch,0) != REG_NOMATCH)
-				{
-					if (pmatch[0].rm_so == -1)
-						break;
-					m = pmatch[0].rm_eo - pmatch[0].rm_so;
-					if (m == 0)
-						break; /* anti-loop */
-					cleaned = 1;
-					matchlen += m;
-					strlncat(buf, ptr, sizeof buf, pmatch[0].rm_so);
-					if (this_word->replace)
-						strlcat(buf, this_word->replace, sizeof buf); 
-					else
-						strlcat(buf, REPLACEWORD, sizeof buf);
-					ptr += pmatch[0].rm_eo;	/* Set pointer after the match pos */
-					memset(&pmatch, 0, sizeof(pmatch));
-				}
-
-				/* All the better to eat you with! */
-				strlcat(buf, ptr, sizeof buf);	
-				memcpy(cleanstr, buf, sizeof cleanstr);
-				memset(buf, 0, sizeof(buf));
-				if (matchlen == stringlen)
-					break;
-			}
-		}
-	}
-
-	cleanstr[511] = '\0'; /* cutoff, just to be sure */
-
-	return (cleaned) ? cleanstr : str;
-}
-
-#ifdef STRIPBADWORDS
-char *_stripbadwords_channel(char *str, int *blocked)
-{
-	return stripbadwords(str, conf_badword_channel, blocked);
-}
-
-char *_stripbadwords_message(char *str, int *blocked)
-{
-	return stripbadwords(str, conf_badword_message, blocked);
-}
-char *_stripbadwords_quit(char *str, int *blocked)
-{
-	return stripbadwords(str, conf_badword_quit, blocked);
-}
-#else
-char *_stripbadwords_channel(char *str, int *blocked)
-{
-	return NULL;
-}
-
-char *_stripbadwords_message(char *str, int *blocked)
-{
-	return NULL;
-}
-char *_stripbadwords_quit(char *str, int *blocked)
-{
-	return NULL;
-}
-#endif
 
 /* Taken from xchat by Peter Zelezny
  * changed very slightly by codemastr

src/s_conf.c
@@ -40,7 +40,6 @@
 #include "h.h"
 #include "inet.h"
 #include "proto.h"
-#include "badwords.h"
 
 #define ircstrdup(x,y) do { if (x) MyFree(x); if (!y) x = NULL; else x = strdup(y); } while(0)
 #define ircfree(x) do { if (x) MyFree(x); x = NULL; } while(0)
@@ -1959,7 +1958,6 @@ void	config_rehash()
 	ConfigItem_listen	 	*listen_ptr;
 	ConfigItem_tld			*tld_ptr;
 	ConfigItem_vhost		*vhost_ptr;
-	ConfigItem_badword		*badword_ptr;
 	ConfigItem_deny_dcc		*deny_dcc_ptr;
 	ConfigItem_allow_dcc		*allow_dcc_ptr;
 	ConfigItem_deny_link		*deny_link_ptr;